7a9659919d2af5e70b348f9ba5b9f6380f8f9d69
Nitesh Sharma <niteshsharma312000@gmail.com>
2023-08-01 17:21:14 +0530

    feat: Support Sentry Tracking (#11)
    * feat: Support Sentry Tracking
    
    * Update ci.yaml
    
    * Remove lint code in ci

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:loggy/loggy.dart';
+import 'package:musie/core/constants/asset_constants.dart';
+import 'package:musie/start/musie_app.dart';
+import 'package:musie/start/setup.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() {
-  runApp(const MainApp());
-}
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final initFuture = Setup.instance.init();
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello Musie!'),
-        ),
+  if (kReleaseMode) {
+    runZonedGuarded(
+      () => SentryFlutter.init(
+        (options) async => await Setup.instance.setupSentry(options),
+        appRunner: () => run(initFuture),
       ),
+      (error, stack) {
+        logError('Unhandled error: $error', null, stack);
+        debugPrintStack(stackTrace: stack);
+      },
+    );
+  } else {
+    runZonedGuarded(
+      () => FlutterError.onError = (details) {
+        FlutterError.presentError(details);
+        logError('FlutterError details: $details');
+
+        run(initFuture);
+      },
+      (error, stack) {
+        logError('Unhandled error: $error', null, stack);
+        debugPrintStack(stackTrace: stack);
+      },
     );
   }
+}
+
+void run(Future<void> future) async {
+  runApp(
+    FutureBuilder(
+      future: future,
+      initialData: null,
+      builder: (context, snapshot) =>
+          snapshot.connectionState == ConnectionState.done
+              ? const MusieApp()
+              : Center(
+                  child: Image.asset(AssetConstants.appIcon),
+                ),
+    ),
+  );
 }
